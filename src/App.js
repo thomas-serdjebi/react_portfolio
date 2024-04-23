05080dfddd053303915b10135da2573a8ecdb8fe
thomas-serdjebi <thomas.serdjebi@laplateforme.io>
2024-04-23 16:09:19 +0200

    FOOTER - création footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import HeaderNavBar from './components/navbars/header';
+import FooterNavBar from './components/navbars/footer';
+import  './styles/body.css';
 
 function App() {
   return (
     <>
       <HeaderNavBar/>
+      <body style={{height: '3000px', backgroundColor: '#eaeaea'}}>
+
+      </body>
+      <FooterNavBar/>
     </>
   );
 }
