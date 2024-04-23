b674d958b5c3718905602cc14c1083c1ebeda428
thomas-serdjebi <thomas.serdjebi@laplateforme.io>
2024-04-23 11:25:04 +0200

    HEADER _ création de la base de la barre

--- src/components/navbars/header.js
+++ src/components/navbars/header.js
@@ -5,25 +5,28 @@ import Profile from "../../assets/profile.jpg"
 function HeaderNavBar() {
   return (<>
     <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          <img src={Profile} width="40" height="40" className="d-inline-block align-top rounded-circle" alt=""></img>
-            Thomas Serdjebi
+      <nav className="navbar align-top navbar-light bg-light ps-5">
+        <a className="navbar-brand d-flex flex-row" href="#">
+          <div><img src={Profile} width="60" height="60" className="d-inline-block align-top rounded-circle me-3" alt=""></img></div>
+            <div className="d-flex flex-column fw-bold">
+              <div>Thomas</div>
+              <div>Serdjebi</div>
+            </div>
         </a>
       </nav>
-      <div className="" id="navbarNav">
-        <ul className="navbar-nav">
+      <div className="fw-bold me-5" id="navbarNav">
+        <ul className="navbar-nav px-5">
           <li className="nav-item">
-            <a className="nav-link font-weight-bold" href="#">ACCUEIL</a>
+            <a className="nav-link " href="#">ACCUEIL</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link font-weight-bold" href="#">A PROPOS</a>
+            <a className="nav-link" href="#">A PROPOS</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link font-weight-bold" href="#">PROJETS</a>
+            <a className="nav-link" href="#">PROJETS</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link font-weight-bold" href="#">CONTACTS</a>
+            <a className="nav-link" href="#">CONTACTS</a>
           </li>
         </ul>
       </div>
